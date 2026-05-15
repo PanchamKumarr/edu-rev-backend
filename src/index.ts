14f958a27745e7e506e0327d14f8240aaf58b57a
PanchamKumarr <114279167+PanchamKumarr@users.noreply.github.com>
2026-05-16 00:07:12 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: true,
   credentials: true
 }));
 
